2751659189d3558408cd160459c0fbc57fe892c4
GoSado <115344930+GoSado@users.noreply.github.com>
2023-09-28 18:09:23 +0100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,13 @@
+function setup(){
+    canvas=createCanvas(300,300);
+    canvas.center();
+    }
+    function draw(){
+    
+    }
+    function preload(){
+        
+    }
+    function takesnapshot(){
+        save("webpage.png");
+    }
